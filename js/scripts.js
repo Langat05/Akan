2c4c1eee594f19de2c282dc0d3689bbcc7ebedd4
Langat <justuslangat78@gmail.com>
2019-11-30 08:38:49 +0300

    modified html and added a function in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,13 @@
+var CC, YY, MM, DD, d, dayValue;
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var cc = parseInt(document.getElementById("CC"));
-var dd = parseInt(document.getElementById("DD"));
-var mm = parseInt(document.getElementById("MM"));
-var male = parseInt(document.getElementById("male"));
-var yy = parseInt(document.getElementById("YY"));
-var female = parseInt(document.getElementById("female"));
 
-function day() {
-    var cc = parseInt(document.getElementById("CC"));
-    var dd = parseInt(document.getElementById("DD"));
-    var mm = parseInt(document.getElementById("MM"));
-    var male = parseInt(document.getElementById("male"));
-    var yy = parseInt(document.getElementById("YY"));
-    var female = parseInt(document.getElementById("female"));
-    var d = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
-    doucument.getElementById("ans") = d;
-
+function validate() {
+  var gender = document.getElementsByTagNameNS("gender");
+  if(document.myform.year.value == "" || document.year.value.length !=4) {
+      alert ("Invalid year");
+      document.myForm.year.focus();
+      return false;
+  }  
 }
-doucument.getElementById("ans") = day();
